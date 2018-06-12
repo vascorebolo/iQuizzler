886d81b6032959bac8a81955965f53d3fe428c6f
Vasco Silva <vasco.s.rebolo@gmail.com>
2018-06-12 18:36:24 +0100

    minor changes

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 class ViewController: UIViewController {
     
     //Place your instance variables here
+    let allQuestions = QuestionBank()
+    var pickedAnswer: Bool = false
+    var questionIndex: Int = 0
     
     
     @IBOutlet weak var questionLabel: UILabel!
@@ -21,25 +24,52 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        questionIndex = 0
+        
+        updateUI()
     }
 
 
     @IBAction func answerPressed(_ sender: AnyObject) {
-  
+        if sender.tag == 1 {
+            pickedAnswer = true
+        } else {
+            pickedAnswer = false
+        }
+        
+        checkAnswer()
     }
     
     
     func updateUI() {
-      
+        let question = allQuestions.list[questionIndex]
+        questionLabel.text = question.questionText
     }
     
 
     func nextQuestion() {
+        if (questionIndex < allQuestions.list.count - 1) {
+            questionIndex += 1
+            print(questionIndex, allQuestions.list.count)
+        } else {
+//            questionIndex = 0;
+            
+        }
         
+        updateUI()
     }
     
     
     func checkAnswer() {
+        let correctAnswer = allQuestions.list[questionIndex].answer
+        
+        if correctAnswer == pickedAnswer {
+            print("good")
+        } else {
+            print("bad")
+        }
+        
+        nextQuestion()
         
     }
     
